753139c060d643566b787e798b31b3f89471ac08
sumansinha1729 <96824471+sumansinha1729@users.noreply.github.com>
2024-09-20 00:38:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
 import { PlayerContext } from './context/PlayerContext'
